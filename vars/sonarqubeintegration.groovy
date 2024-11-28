c3c845aab8fe50fb7c4cec7d2780b96ff36d18c5
pramod348 <144993118+pramod348@users.noreply.github.com>
2024-11-28 14:51:05 +0530

    Update sonarqubeintegration.groovy

--- vars/sonarqubeintegration.groovy
+++ vars/sonarqubeintegration.groovy
@@ -1,8 +1,19 @@
 // vars/sonarQubeIntegration.groovy
 def call() {
-    // Ensure SonarQube environment variables are set from the .env file
-    withSonarQubeEnv('SonarQube') {
-        // Run the SonarQube analysis
-        sh 'mvn clean install sonar:sonar -Dsonar.login=${SONAR_TOKEN}'
+    // Check if SonarQube URL and Token are available
+    if (!env.SONAR_URL || !env.SONAR_TOKEN) {
+        error "SONAR_URL and SONAR_TOKEN must be defined in the .env file"
+    }
+
+    // Configure the SonarQube scanner for Jenkins
+    withEnv(["SONAR_HOST_URL=${env.SONAR_URL}", "SONAR_LOGIN=${env.SONAR_TOKEN}"]) {
+        // Run the SonarQube scan using Maven (or adjust based on your build tool)
+        sh """
+            mvn clean install sonar:sonar \
+            -Dsonar.projectKey=${env.SONAR_PROJECT_KEY} \
+            -Dsonar.projectName=${env.SONAR_PROJECT_NAME} \
+            -Dsonar.host.url=${env.SONAR_URL} \
+            -Dsonar.login=${env.SONAR_TOKEN}
+        """
     }
 }
